d31e71aedabba9a993741082406acfbe668b4aae
Suverov Dmitriy <suverov.dmitriy@gmail.com>
2018-06-20 12:35:24 +0300

    update readme

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const artistsController = require('./controllers/artists');
 
 const app = express();
 const db = require('./db');
+const PORT = 27017;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extend: true}));
@@ -14,21 +15,17 @@ app.get('/', (req, res) => {
     res.send('Hello there');
 });
 
+// TODO move routes to separate file, then connect via express.router
 app.get('/artists', artistsController.all);
-
 app.get('/artists/:id', artistsController.findById);
-
 app.post('/artists', artistsController.create);
-
 app.put('/artists/:id', artistsController.update);
-
 app.delete('/artists/:id', artistsController.delete);
 
-db.connect('mongodb://localhost:27017/loftblog_api', (err) => {
+db.connect(`mongodb://localhost:${PORT}/loftblog_api`, (err) => {
     if (err) console.log(err);
     app.listen(PORT, () => {
         console.log(`Api started on the port: ${PORT} - http://localhost:${PORT}`);
-        console.log(`Api started on the port: ${PORT} - http://localhost:${PORT}/artists`);
     });
 });
 
